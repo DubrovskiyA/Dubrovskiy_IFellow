2eacb273d975cb8ad3817b720440a7c0c322d640
DubrovskiyArtem <artemdubrovskiysar@gmail.com>
2024-09-08 20:06:03 +0400

    Init commit

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,3 +1,33 @@
+import org.junit.jupiter.api.*;
+
 public class RunTest {
-    //its RunTest origin
+
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("BeforeAll!");
+    }
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("AfterAll");
+    }
+
+    @Test
+    @DisplayName("Первый тест")
+    @Tag("Positive")
+    public void test1(){
+        System.out.println("Test_1");
+        int x=9;
+        int y=2;
+        int z=x%y;
+        Assertions.assertTrue(y>z);
+    }
+    @Test
+    @DisplayName("Второй тест")
+    @Tag("Negative")
+    public void test2(){
+        System.out.println("Test_2");
+        String originName="Alex";
+        String nameFromAnotherPlace="Bill";
+        Assertions.assertEquals(originName,nameFromAnotherPlace,"Имена не совпадают!");
+    }
 }
